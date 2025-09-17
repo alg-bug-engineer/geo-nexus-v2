8ee77966a1751e4ddbd6d27cd4950cd642fbcdc9
alg-bug-engineer <646184101@qq.com>
2025-09-17 16:50:50 +0800

    feat: Integrate Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react'; // 1. 从刚安装的包里导入Analytics组件
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics /> {/* 2. 在这里添加Analytics组件 */}
     </Layout>
   );
 }
